3a8a3bb3eb49422c50926ff2fd7963d48616b828
Daniel Ransom <dantheman0207@users.noreply.github.com>
2024-03-03 18:16:07 +0100

    try this

---
+++ cmd/cli/chat_commands.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/FyodorovAI/fyodorov-cli-tool/internal/api-client"
+	"github.com/FyodorovAI/fyodorov-cli-tool/internal/common"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(chatCmd)
+}
+
+// Fyodorov commands
+var chatCmd = &cobra.Command{
+	Use:   "chat",
+	Short: "Manage Fyodorov configuration",
+	Run: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
+		config, err := common.LoadConfig[common.Config](configPath)
+		if err != nil || config == nil || config.GagarinURL == "" {
+			fmt.Println("Enter Gagarin URL:")
+			input, _ := reader.ReadString('\n')
+			gagarinURL = strings.TrimSpace(input)
+			if config == nil {
+				config = &common.Config{}
+			}
+			config.GagarinURL = gagarinURL
+		}
+		client := api.NewAPIClient(config, gagarinURL)
+		err = client.Authenticate()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Unable to authenticate with this config")
+			initConfig(cmd, args)
+		}
+		if len(args) > 0 {
+			agentName := args[0]
+		}
+	},
+}
